b0d2e30181ee8a87b7e9c4185efac416faf1d714
Prodragnix <connect.neilkumar@gmail.com>
2024-10-08 19:05:11 +0100

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,7 @@
+num1=int(input('Please input your first value: '))
+num2=int(input('Please input your second value: '))
+num3=int(input('Please input your third value: '))
+num1, num2, num3 = num3, num1, num2
+print(num1)
+print(num2)
+print(num3)
